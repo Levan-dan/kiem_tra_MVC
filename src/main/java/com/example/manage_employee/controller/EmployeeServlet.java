df6f46a0c9b345e573c00b252b4041fdcfba1f5e
vandan <levandanhh2@gmail.com>
2024-12-10 14:21:49 +0700

    hienThiSanPham

--- src/main/java/com/example/manage_employee/controller/EmployeeServlet.java
+++ src/main/java/com/example/manage_employee/controller/EmployeeServlet.java
@@ -1,4 +1,26 @@
 package com.example.manage_employee.controller;
 
-public class EmployeeServlet {
+import com.example.manage_employee.service.EmployeeService;
+import com.example.manage_employee.service.EmployeeServiceImpl;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "employeeServlet", urlPatterns = "/employee")
+
+public class EmployeeServlet extends HttpServlet {
+    private final EmployeeService employeeService = new EmployeeServiceImpl();
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response){
+
+    }
+
 }
